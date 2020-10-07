8b92e9e43e3c94902985b84a3845588c83e8dc15
Justin Strate <jstrat1.618@gmail.com>
2020-10-06 19:46:22 -0600

    Update Text Analytics Portion

--- R/shiny_job_app/app.R
+++ R/shiny_job_app/app.R
@@ -8,191 +8,213 @@ library(shiny)
 
 
 ui <- shinyUI(
-    navbarPage("Jobs Database",
-               tabPanel(
-                   "Filter Data",
-                   sidebarLayout(
-                       sidebarPanel(
-                           sliderInput("num_rows",
-                                       "Number of Rows:",
-                                       min = 1,
-                                       max = 10,
-                                       value = 5),
-                           textInput("title_contains",label = 
-                                         'Title Contains (Not case sensitive)'),
-                           textInput("summary_contains",label = 
-                                         'Job Summary Contains (Not case sensitive)'),
-                           actionButton('refresh', "Refresh")
-                       ),
-                       
-                       # Show a plot of the generated distribution
-                       
-                       tabPanel("main_df", tableOutput("main_df"))
-                   )
-               ),
-            tabPanel("View Data by Location", 
-                        sidebarLayout(
-                            sidebarPanel(
-                                sliderInput("num_cities",
-                                            "Number of Cities:",
-                                            min = 2,
-                                            max = 25,
-                                            value = 5)),
+  navbarPage("Jobs Database",
+             tabPanel(
+               "Filter Data",
+               sidebarLayout(
+                 sidebarPanel(
+                   sliderInput("num_rows",
+                               "Number of Rows:",
+                               min = 1,
+                               max = 10,
+                               value = 5),
+                   textInput("title_contains",label = 
+                               'Title Contains (Not case sensitive)'),
+                   textInput("summary_contains",label = 
+                               'Job Summary Contains (Not case sensitive)'),
+                   actionButton('refresh', "Refresh")
+                 ),
+                 
+                 # Show a plot of the generated distribution
+                 
+                 tabPanel("main_df", tableOutput("main_df"))
+               )
+             ),
+             tabPanel("View Data by Location", 
+                      sidebarLayout(
+                        sidebarPanel(
+                          sliderInput("num_cities",
+                                      "Number of Cities:",
+                                      min = 2,
+                                      max = 25,
+                                      value = 5)),
                         mainPanel(plotOutput("city_plt")))),
-            
-            tabPanel("View Data Over Time",
-                     sidebarLayout(
-                         sidebarPanel(
-                             sliderInput("num_dum",
-                                         "Number of Cities:",
-                                         min = 2,
-                                         max = 25,
-                                         value = 5)),
-                         mainPanel(plotOutput("ts_plt")))),
-            tabPanel("Text Analytics",
-                     sidebarLayout(
-                         sidebarPanel(
-                             sliderInput("num_words",
-                                         "Number of Words",
-                                         min = 2,
-                                         max = 25,
-                                         value = 10),
-                             h4("Compare Words (not case sensetive)"),
-                             textInput("word1", label="Enter Word"),
-                             textInput("word2", label="Enter Word"),
-                             textInput("word3", label="Enter Word"),
-                             textInput("word4", label="Enter Word")),
-                         mainPanel(plotOutput("word_sum_plt"))))
-    )
+             
+             tabPanel("View Data Over Time",
+                      sidebarLayout(
+                        sidebarPanel(
+                          sliderInput("num_dum",
+                                      "Number of Cities:",
+                                      min = 2,
+                                      max = 25,
+                                      value = 5)),
+                        mainPanel(plotOutput("ts_plt")))),
+             tabPanel("Text Analytics",
+                      sidebarLayout(
+                        sidebarPanel(
+                          sliderInput("num_words",
+                                      "Number of Words",
+                                      min = 2,
+                                      max = 25,
+                                      value = 10),
+                          textInput("word1",label = 
+                                      'Enter Word (Not case sensitive)'),
+                          textInput("word2",label = 
+                                      'Enter Word (Not case sensitive)'),
+                          textInput("word3",label = 
+                                      'Enter Word (Not case sensitive)'),
+                          textInput("word4",label = 
+                                      'Enter Word (Not case sensitive)')),
+                        mainPanel(plotOutput("word_sum_plt"),
+                                  plotOutput("unigram_plt"))))
+  )
 )
 
 
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    #################################################################################################
-    #Get Data
-    #################################################################################################
-    conn <- dbConnect(SQLite(), '../../venv/data/jobs_database.db')
+  #################################################################################################
+  #Get Data
+  #################################################################################################
+  conn <- dbConnect(SQLite(), '../../venv/data/jobs_database.db')
+  
+  query <- dbSendQuery(conn, "SELECT * FROM main_jobs")
+  
+  dat <- dbFetch(query)
+  
+  dat <- as_tibble(dat)
+  
+  dbDisconnect(conn)
+  
+  
+  # Data Cleaning
+  dat$pulled <- as.Date(dat$pulled, format = '%m/%d/%Y')
+  
+  newdata <- reactive({
     
-    query <- dbSendQuery(conn, "SELECT * FROM main_jobs")
+    input$refresh
+    isolate({
+      
+      newdat <- dat
+      
+      title_contains <- tolower(input$title_contains)
+      summary_contains <- tolower(input$summary_contains)
+      
+      if(!title_contains %in% c(""," ")){
+        newdat <- filter(newdat, grepl(title_contains, tolower(dat$title)))
+      }
+      
+      if(!summary_contains  %in% c(""," ")){
+        newdat <-  filter(newdat, grepl(summary_contains, tolower(newdat$summary)))
+      }
+      
+      return(newdat)})
     
-    dat <- dbFetch(query)
-    
-    dat <- as_tibble(dat)
-    
-    dbDisconnect(conn)
-    
-    
-    # Data Cleaning
-    dat$pulled <- as.Date(dat$pulled, format = '%m/%d/%Y')
-    
-    newdata <- reactive({
-        
-        input$refresh
-        isolate({
-        
-        newdat <- dat
-        
-        title_contains <- tolower(input$title_contains)
-        summary_contains <- tolower(input$summary_contains)
-        
-        if(!title_contains %in% c(""," ")){
-            newdat <- filter(newdat, grepl(title_contains, tolower(dat$title)))
-        }
-        
-        if(!summary_contains  %in% c(""," ")){
-            newdat <-  filter(newdat, grepl(summary_contains, tolower(newdat$summary)))
-        }
-        
-        return(newdat)})
-        
-    })
-    
-    
-    output$main_df <- renderTable(
+  })
+  
+  
+  output$main_df <- renderTable(
     
     newdata() %>% 
-        dplyr::select(title, link, location, pulled) %>%
-        rename(Title = title,
-               Location = location, 
-               `Date Pulled` = pulled) %>%
-       head(input$num_rows)
-    )
+      dplyr::select(title, link, location, pulled) %>%
+      rename(Title = title,
+             Location = location, 
+             `Date Pulled` = pulled) %>%
+      head(input$num_rows)
+  )
+  
+  output$city_plt <- renderPlot({
+    city_plt <- 
+      newdata() %>%
+      count(location) %>%
+      arrange(desc(n)) %>%
+      filter(!(location %in% c(',', "", " ")) & !is.na(location)) %>%
+      head(input$num_cities) %>%
+      ggplot(aes(reorder(location, n), n))+
+      geom_bar(stat='identity')+
+      labs(x = "", y = "Number of Postings", title ='Job Posts by City')+   
+      coord_flip()
     
-    output$city_plt <- renderPlot({
-        city_plt <- 
-            newdata() %>%
-            count(location) %>%
-            arrange(desc(n)) %>%
-            filter(!(location %in% c(',', "", " ")) & !is.na(location)) %>%
-            head(input$num_cities) %>%
-            ggplot(aes(reorder(location, n), n))+
-                geom_bar(stat='identity')+
-                labs(x = "", y = "Number of Postings", title ='Job Posts by City')+   
-                coord_flip()
-            
-        print(city_plt)
-        })
+    print(city_plt)
+  })
+  
+  output$ts_plt <- renderPlot({
+    ts_plt <- 
+      newdata() %>%
+      count(pulled) %>%
+      ggplot(aes(pulled, n))+
+      geom_line()+
+      geom_point()+
+      labs(x = "Date Pulled", y = "Number of Postings", 
+           title ='Job Postings over Time')
     
-    output$ts_plt <- renderPlot({
-        ts_plt <- 
-            newdata() %>%
-            count(pulled) %>%
-            ggplot(aes(pulled, n))+
-            geom_line()+
-            geom_point()+
-            labs(x = "Date Pulled", y = "Number of Postings", 
-                 title ='Job Postings over Time')
+    print(ts_plt)
+  })
+  
+  output$word_sum_plt <- renderPlot({
+    word_df <-
+      newdata() %>%
+      select(summary) %>%
+      #Remove html tags
+      mutate(summary = gsub('<.*?>', '', summary)) %>%
+      unnest_tokens(word, summary) %>%
+      # Filter out stop-words but BE CAREFUL- R is a stop-word
+      anti_join(tidytext::stop_words %>%
+                  filter(word != 'r'), by='word') %>%
+      filter(!word  %in% c('nbsp', 'rsquo', 'data', 'science'))%>%
+      count(word) %>%
+      arrange(desc(n))
+    
+    word_plt <- 
+      word_df %>%
+      head(input$num_words) %>%
+      ggplot(aes(reorder(word, n), n))+
+      geom_bar(stat = 'identity')+
+      labs(x="", y="Number of Times Used", 
+           title='Most Common Words in Job Description')+
+      coord_flip()
+    
+    
+    
+    print(word_plt)
+  })
+  
+  output$unigram_plt <- renderPlot({
+   
+    user_words <- tolower(c(input$word1, input$word2, input$word3, input$word4))
+    
+    if(sum(user_words == "")>2){
+      return(NULL)
+      } else{
         
-        print(ts_plt)
-    })
+    udf <-
+      newdata() %>%
+      select(summary) %>%
+      #Remove html tags
+      mutate(summary = gsub('<.*?>', '', summary)) %>%
+      unnest_tokens(word, summary) %>%
+      # Filter out stop-words but BE CAREFUL- R is a stop-word
+      filter(word %in% user_words)%>%
+      count(word) %>%
+      arrange(desc(n))
     
-    output$word_sum_plt <- renderPlot({
-        word_df <-
-            newdata() %>%
-            select(summary) %>%
-            #Remove html tags
-            mutate(summary = gsub('<.*?>', '', summary)) %>%
-            unnest_tokens(word, summary) %>%
-            # Filter out stop-words but BE CAREFUL- R is a stop-word
-            anti_join(tidytext::stop_words %>%
-                          filter(word != 'r'), by='word') %>%
-            filter(!word  %in% c('nbsp', 'rsquo', 'data', 'science'))%>%
-            count(word) %>%
-            arrange(desc(n))
-        
-       word_plt <- 
-            word_df %>%
-            head(input$num_words) %>%
-            ggplot(aes(reorder(word, n), n))+
-            geom_bar(stat = 'identity')+
-            labs(x="", y="Number of Times Used", 
-                 title='Most Common Words in Job Description')+
-            coord_flip()
-        
-       words <- tolower(c(input$word1, input$word2, input$word3,input$word4))
-      
-        if(input$word1 == '' | input$word2 == ''){
-            print(word_plt)
-        }else{
-            plt_comp <- 
-                word_df %>%
-                filter(word %in% words) %>%
-                ggplot(aes(reorder(word, n), n))+
-                geom_bar(stat = 'identity')+
-                labs(x="", y="Number of Times Used", 
-                     title=paste(input$word1, "and", input$word2))+
-                coord_flip()
-            
-            plt <- gridExtra::grid.arrange(word_plt, plt_comp, nrow=2)
-            
-            print(plt)
-        }
-        
-    })
+    u_plt <- 
+      udf %>%
+      head(input$num_words) %>%
+      ggplot(aes(reorder(word, n), n))+
+      geom_bar(stat = 'identity')+
+      labs(x="", y="Number of Times Used", 
+           title='')+
+      coord_flip()
     
     
+    
+    print(u_plt)
+    }
+  })
+  
+  
 }
 
 # Run the application 
